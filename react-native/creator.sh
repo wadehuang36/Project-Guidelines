ba826053248c05d5241d0d32b19ead0cd06c5270
Wade <wadehuang36@gmail.com>
2019-06-14 18:25:24 -0400

    fix creator bug

--- react-native/creator.sh
+++ react-native/creator.sh
@@ -13,12 +13,13 @@ cd $ProjectName
 # create folders and move default files
 mkdir app app/src app/src/scenes app/src/services app/src/utils app/res/ app/res/images app/res/fonts app/test
 mkdir build .vscode
-mv __tests__/* app/test
+mv __tests__/App-test.tsx app/test/app-test.tsx
 rm App.tsx
 rm -r __tests__
 
-# change the path of import App on index.js
+# change the path of import App on index.js and app.test.tsx
 sed -i "s/'\.\/App'/'.\/app\/src\/scenes\/app'/" index.js
+sed -i "s/'\..\/App'/'..\/src\/scenes\/app'/" app/test/app-test.tsx
 
 # install packages
 npm install -D eslint prettier eslint-plugin-react eslint-config-prettier eslint-plugin-prettier @typescript-eslint/eslint-plugin @typescript-eslint/parser
@@ -34,20 +35,20 @@ sed -i "/\"test\": \"jest\"/ s/.*/${NPM_SCRIPTS}&/" package.json
 # add pre-commit
 PRECOMMIT='"pre-commit": [\n"lint",\n"test",\n"build"],\n'
 PRECOMMIT=$(echo $PRECOMMIT | sed 's/\"/\\\"/g')
-sed "/\"dependencies\": {/ s/.*/${PRECOMMIT}\n&/" package.json
+sed -i "/\"dependencies\": {/ s/.*/${PRECOMMIT}\n&/" package.json
 
-## format package.json
+# format package.json
 npx prettier package.json --write
-npx eslint index.js --fix
+npx eslint **/*.tsx --fix
 
 # download files
-wget -q -o=app/src/scenes/app.tsx https://raw.githubusercontent.com/swarmnyc/SWARM-Project-Guidelines/master/react-native/app.tsx
-wget -q -o=app/res/string-res.tsx https://raw.githubusercontent.com/swarmnyc/SWARM-Project-Guidelines/master/react-native/string-res.tsx
+wget -q -P app/src/scenes https://raw.githubusercontent.com/swarmnyc/SWARM-Project-Guidelines/master/react-native/app.tsx
+wget -q -P app/res https://raw.githubusercontent.com/swarmnyc/SWARM-Project-Guidelines/master/react-native/string-res.ts
 wget -q https://raw.githubusercontent.com/swarmnyc/SWARM-Project-Guidelines/master/react-native/.editorconfig
 wget -q https://raw.githubusercontent.com/swarmnyc/SWARM-Project-Guidelines/master/react-native/.eslintrc.js
-wget -q -o=.vscode/settings.json https://raw.githubusercontent.com/swarmnyc/SWARM-Project-Guidelines/master/react-native/settings.json
+wget -q -P .vscode https://raw.githubusercontent.com/swarmnyc/SWARM-Project-Guidelines/master/react-native/settings.json
 
-setup git
+#setup git
 git init
 git add .
 git commit -m "initialize project"
